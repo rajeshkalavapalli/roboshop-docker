c05d2068e6465f2638cb7d4732320112167e2d50
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-05-14 19:13:42 +0530

    catalogue docker file

---
+++ catalogue/server.js
@@ -0,0 +1,41 @@
+const mongoClient = require('mongodb').MongoClient;
+const mongoObjectID = require('mongodb').ObjectID;
+const bodyParser = require('body-parser');
+const express = require('express');
+const pino = require('pino');
+const expPino = require('express-pino-logger');
+
+const logger = pino({
+    level: 'info',
+    prettyPrint: false,
+    useLevelLabels: true
+});
+const expLogger = expPino({
+    logger: logger
+});
+// imagine this as developer changing the code
+// MongoDB
+var db;
+var collection;
+var mongoConnected = false;
+
+const app = express();
+
+app.use(expLogger);
+
+app.use((req, res, next) => {
+    res.set('Timing-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/health', (req, res) => {
+    var stat = {
+        app: 'OKK',
+        mongo: mongoConnected
+    };
+    res.json(stat);
+});
